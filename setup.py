f2b10fe5842f1b426a8611ec239d0878768d5aaa
Ruturaj Vaidya <ruturajkvaidya@ku.edu>
2019-11-22 09:52:02 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' Great stories and great visual effects',
+    author='Ruturaj Kiran Vaidya',
+    license='MIT',
+)
